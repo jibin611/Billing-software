ead6ba4fc31e709b7a39710d22a13bc4efd75c34
Jibin Raj K <jibinraj619@gmail.com>
2022-02-03 09:13:18 +0530

    read , print and save any number of customer bill

--- bill.c
+++ bill.c
@@ -15,76 +15,51 @@ void print_bill( bill_info *bill, char *name , int limit);
 int main()
 {
 
-    char (*filename)[15], choice2;
+    char choice2;
+    FILE *fptr;
     int option, j = 0;
-    filename = malloc( (j + 1) * sizeof(char*) );
     do{
-	printf("1. Enter customer bill\n2. Print all bill\n3. print bill by name search\n4. exit\nselect an option: ");
-	scanf("%d", &option);
-	switch ( option )
+	char name[15];
+	printf("Enter the customer name: ");
+	scanf("\n%[^\n]", name);
+
+	bill_info *bill;  //structure pointer declared
+	bill = calloc( 1, sizeof(bill_info) );  //allocate memmory for one structure variable
+	char choice;
+	int i = 0, flag = 1;
+
+	do{
+	    //read values for structure members
+	    printf("Enter the item name: ");
+	    scanf("\n%[^\n]", bill[i].item); 
+	    printf("Enter quantity: ");
+	    scanf("%d", &bill[i].quantity);
+	    printf("Enter price: ");
+	    scanf("%f", &bill[i].price);
+	    bill[i].qty_price = bill[i].quantity * bill[i].price; 
+
+	    printf("press ENTER to continue\npress 'q' to stop\n");
+	    getchar();
+	    scanf("%c", &choice);
+	    bill = realloc( bill, ( i + 2 ) * sizeof( bill_info ) );  //memory incresed to store next item details
+	    i++;
+	}while( choice != 'q' );  //using do while, if condition is true repeat the process
+
+	print_bill( bill, name, i );  //call function
+
+	char choice1, filename[15];
+	printf("Do you want to save? [y/n]\n");
+	getchar();
+	scanf("%c", &choice1);
+	if ( choice1 == 'y' )
 	{
-	    case 1:
-		{
-		    char name[15];
-		    printf("Enter the customer name: ");
-		    scanf("\n%[^\n]", name);
-
-		    bill_info *bill;  //structure pointer declared
-		    bill = calloc( 1, sizeof(bill_info) );  //allocate memmory for one structure variable
-		    char choice;
-		    int i = 0, flag = 1;
-
-		    do{
-			//read values for structure members
-			printf("Enter the item name: ");
-			scanf("\n%[^\n]", bill[i].item); 
-			printf("Enter quantity: ");
-			scanf("%d", &bill[i].quantity);
-			printf("Enter price: ");
-			scanf("%f", &bill[i].price);
-			bill[i].qty_price = bill[i].quantity * bill[i].price; 
-
-			printf("press ENTER to continue\npress 'q' to stop\n");
-			getchar();
-			scanf("%c", &choice);
-			bill = realloc( bill, ( i + 2 ) * sizeof( bill_info ) );  //memory incresed to store next item details
-			i++;
-		    }while( choice != 'q' );  //using do while, if condition is true repeat the process
-
-		    print_bill( bill, name, i );  //call function
-
-		    char choice1;
-		    printf("Do you want to save? [y/n]\n");
-		    getchar();
-		    scanf("%c", &choice1);
-		    if ( choice1 == 'y' )
-		    {
-			printf("Enter the filename to save bill: ");
-			scanf("\n%s", filename[j]);
-			strcat(filename[j], ".txt");
-			freopen( filename[j], "w", stdout );  //save data to file
-			print_bill( bill, name, i );  //call function
-			freopen( "/dev/tty", "w",stdout );  //goto stdout from file
-			filename = realloc( filename, (++j + 1) * sizeof(char*) );
-			printf("saved successfully\n");
-		    }
-		    break;
-		}
-	    case 3:
-		{
-		    int option1;
-		    for ( int i = 0; i < j; i++ )
-			printf("%d. %s\n", i + 1, filename[i]);
-		    printf("select one filename :");
-		    scanf("%d", &option1);
-		    FILE *fptr = fopen( filename[ option1 - 1 ], "w" );
-		    char ch = fgetc( fptr );
-		    while( ch != EOF )
-			fputc( ch, stdout );
-		    break;
-		}
-	    case 4:
-		return 0;
+	    printf("Enter the filename to save bill: ");
+	    scanf("\n%s", filename);
+	    strcat(filename, ".txt");
+	    fptr = freopen( filename, "w", stdout );  //save data to file
+	    print_bill( bill, name, i );  //call function
+	    freopen( "/dev/tty", "w",stdout );  //goto stdout from file
+	    printf("saved successfully\n");
 	}
 	printf("Do you want to continue? [y/n]: ");
 	scanf("\n%c", &choice2);
